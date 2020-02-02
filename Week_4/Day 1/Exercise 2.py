67e61f3be1b92542f87c1d9c63a5ff4a4c4ef9ac
Jenni1985 <59572065+Jenni1985@users.noreply.github.com>
2020-02-02 14:05:54 +0200

    Add files via upload

---
+++ Week_4/Day 1/Exercise 2.py
@@ -0,0 +1,26 @@
+name = "jenni"
+num = 1
+print(num)
+print("hello" + name)
+
+
+name = "jon"
+name.upper()
+name.lower()
+name.capitalize()
+sentence.title()
+
+Lists.Arrays [1,2,3,4,1,2,6]
+Dictionary - Javascript objects
+
+SETS {1,2,3,4,6}
+TUPPLE{}
+
+letters = ["a", "b", "c"]
+if name == "jon":
+    pass
+elif name == "dave":
+
+first_name = "jenni"
+last_name = "chaffe"
+print( first_name +  last_name)    
