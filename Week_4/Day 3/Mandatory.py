1af976d264b0b16fbd6521b9670cee878be555d2
Jenni1985 <59572065+Jenni1985@users.noreply.github.com>
2020-02-05 13:52:59 +0200

    Add files via upload

---
+++ Week_4/Day 3/Mandatory.py
@@ -0,0 +1,77 @@
+#1. a = 15
+# b = 8
+#
+# if b<a:
+#     print("Hello World")
+
+
+#2.numero = input('Dame tres numeros!')
+# Lista = ["25", "78", "87"]
+# print(max([int(num) for num in Lista]))
+
+
+# 3.month = int(input("Please enter the number of month (1-12) "))
+# spring = [3,4,5]
+# summer = [6,7,8]
+# autumn = [9,10,11]
+# winter = [12,1,2]
+#
+# if month in spring:
+#      print("Spring runs from March (3) to May (5)")
+#
+# elif month in summer:
+#      print("Summer runs from June (6) to August (8)")
+#
+# elif month in autumn:
+#      print("Autumn runs from September (9) to November (11)")
+#
+# elif month in winter:
+#     print("Winter runs from December (12) to February (2)")
+
+
+#6.for i in range(1,21):
+ #    print(i)
+
+# 7.numbers = []
+# for i in range(0, 100000000):
+#     numbers.append(i)
+# for j in numbers:
+#      print(j)
+
+#9 star = ""
+#for i in range(0,5):
+    # star = star + "*"
+     #print(star)
+
+#10. Write a program that returns the index of the first occurrence of an item in a list.
+#list = [4,5,6,6,3,4,2]
+
+#for index, value in enumerate(list):
+#     print(index)
+ #    break
+
+#11.Write a little program that concatenate two lists together without using the + sign.
+
+# list1 = [34,3423,'hello','world',56]
+# list2 = [34,3423,'you','are',56,'my']
+#
+# joined = [*list1,*list2]
+# print(joined)
+
+# 12.user_input = input("Please enter your topping for the pizza, if you want to end - type 'quit' ")
+# if (user_input == 'quit'):
+#     print("I dont want")
+#     print('You added ' + user_input + ' to your pizza')
+
+#A movie theater charges different ticket prices depending on a person’s age . If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15 .
+#Write a loop in which you ask users their age, and then tell them the cost of their movie ticket .
+
+# 13. age = int(input('Please enter your age'  '6'))
+# if age == 6:
+#     if 3 < age and age < 12:
+#         print("Your ticket will be 5$")
+# elif age > 12:
+#     print("Your ticket will be 10$")
+
+
+
